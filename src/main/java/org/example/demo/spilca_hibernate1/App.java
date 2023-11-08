372acef84a6defd89e01caffa782550b4da5a527
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-08 19:18:14 -0300

    init

---
+++ src/main/java/org/example/demo/spilca_hibernate1/App.java
@@ -0,0 +1,13 @@
+package org.example.demo.spilca_hibernate1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
