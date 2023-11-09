e9c76cbf064bb787a0990bec54f053c3c68d1f66
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-09 20:19:19 -0300

    entity done

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,9 +1,33 @@
 package entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Product {
+	
+	@Id
+	private long id;
+	
+	private String name;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 	
 	
 }
