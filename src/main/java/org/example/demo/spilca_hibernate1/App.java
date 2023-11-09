0f8ff7fbcd462e88029e96373a2bb109d7136e99
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-09 20:03:19 -0300

    entity manager factory

--- src/main/java/org/example/demo/spilca_hibernate1/App.java
+++ src/main/java/org/example/demo/spilca_hibernate1/App.java
@@ -1,6 +1,8 @@
 package org.example.demo.spilca_hibernate1;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 /**
  * Setting up HIBERNATE!
@@ -10,8 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("YourPersistenceUnitName");
         
-    	EntityManager em = null;
+    	EntityManager em = emf.createEntityManager(); // represent the CONTEXT
     	
     	em.getTransaction().begin();
     	
